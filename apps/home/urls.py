a9e148f4e36354d740759dace65da51e891c99f5
awsumal <aliabbasthakkar@gmail.com>
2022-02-25 16:13:54 +0530

    1. Removed unnecessary unwanted text 2. Reworked landing.html, changed button styles, video playback issues, and added a view in views.py 3. Mitigated URLs starting has to be at 127.0.0.1:8000/landing

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,7 +1,4 @@
 # -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
 
 from django.urls import path, re_path
 from apps.home import views
@@ -9,7 +6,10 @@ from apps.home import views
 urlpatterns = [
 
     # The home page
+
+    #path('', views.index, name='home'),
     path('', views.index, name='home'),
+    path('landing', views.landing),
 
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
