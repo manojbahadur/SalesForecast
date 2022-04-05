527f52f382858d5b7dcf74fce3b434c518035860
Manoj Bahadur <47881066+manojbahadur@users.noreply.github.com>
2022-04-05 21:57:45 +0530

    changes

--- apps/home/views.py
+++ apps/home/views.py
@@ -8,6 +8,7 @@ from django.template import loader
 from django.urls import reverse
 from django.shortcuts import render
 from django.http import JsonResponse
+from json import dumps
 
 @login_required(login_url="/login/")
 def index(request):
@@ -58,7 +59,7 @@ def pie_chart(request):
         'data': data,
     })'''
 
-@login_required(login_url="/login/")
+
 def population_chart(request):
     labels = ["a","b","c","d","e","f","g","h"]
     data1 = [100,200,120,230,123,100,123,235]
@@ -67,8 +68,8 @@ def population_chart(request):
     for entry in queryset:
         labels.append(entry['country__name'])
         data.append(entry['country_population'])'''
-    
-    return JsonResponse(data={
+    context = {
         'labels': ["a","b","c","d","e","f","g","h"],
         'data': [100,200,120,230,123,100,123,235],
-    })
+    }
+    return render(request, 'home/index.html', context)
